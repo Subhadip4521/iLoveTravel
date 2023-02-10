ef4a9f6eb0e525518c823b6d60da5059b0436d67
SubhadipDas <subhadipdas4521@gmail.com>
2023-02-11 03:09:30 +0530

    new commit2 11-02-23

--- home/urls.py
+++ home/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name='home'),
     path("signup/", views.signupUser, name='signup'),
-    path("login/", views.loginUser, name='login'),
-    path("logout", views.logoutUser ,name='logout'),
+    path("login", views.loginUser, name='login'),
+    path("logout/", views.logoutUser ,name='logout'),
     
     path("about/", views.about, name='about'),
     path("contact/", views.contact, name='contact'),
